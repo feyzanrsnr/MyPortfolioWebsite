a2458de876991bed0bf79d91a78bd7f19c32706c
feyzanursener <senerffeyza@gmail.com>
2025-07-11 16:10:10 +0300

    footer hatası düzeltildi

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from './components/Home/Home'
 import About from './components/About/About'
 import Projects from './components/Projects/Projects'
 import Contact from './components/Contact/Contact'
-import Footer from './components/Footer/footer'
+import Footer from './components/Footer/Footer'
 import { useState, useEffect } from 'react'
 
 function App() {
